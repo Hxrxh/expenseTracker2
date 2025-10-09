11e2cf6971bd5e6764028cc70bd000a36cad59b9
Hxrxh <harshbhardwaj4556>
2025-10-10 02:32:02 +0530

    transaction used

--- backend/controller/expenseController.js
+++ backend/controller/expenseController.js
@@ -1,29 +1,38 @@
 const { userTable } = require("../models");
 const expenseTable = require("../models/expenseModel");
-
+const sequelize = require("../utils/db-connection");
 const addExpense = async (req, res) => {
+  const t = await sequelize.transaction();
   try {
     const { id: userId } = req.user;
     console.log(userId);
     const { amount, desc, category } = req.body;
 
-    const addedExpense = await expenseTable.create({
-      expenseamount: amount,
-      description: desc,
-      category: category,
-      userId: userId,
-    });
+    const addedExpense = await expenseTable.create(
+      {
+        expenseamount: amount,
+        description: desc,
+        category: category,
+        userId: userId,
+      },
+      {
+        transaction: t,
+      }
+    );
     await userTable.increment(
       { totalExpense: amount },
       {
         where: {
           id: userId,
         },
+        transaction: t,
       }
     );
+    await t.commit();
     res.status(201).json(addedExpense);
   } catch (error) {
     console.log(error);
+    await t.rollback();
     res.status(500).json({ message: error.message });
   }
 };
@@ -45,17 +54,33 @@ const getExpenseData = async (req, res) => {
 };
 
 const deleteExpense = async (req, res) => {
+  const t = await sequelize.transaction();
   try {
     const { id: userId } = req.user;
     const { id } = req.params;
+    const user = await userTable.findByPk(userId, { transaction: t });
+    const expenseData = await expenseTable.findOne({
+      where: {
+        id: id,
+        userId: userId,
+      },
+      transaction: t,
+    });
+
+    user.totalExpense -= expenseData.expenseamount;
+    await user.save({ transaction: t });
     const deletedData = await expenseTable.destroy({
       where: {
         id: id,
         userId: userId,
       },
+      transaction: t,
     });
+
+    await t.commit();
     res.status(200).send("Successfully deleted");
   } catch (error) {
+    await t.rollback();
     res.status(500).json({ err: error.message });
   }
 };
